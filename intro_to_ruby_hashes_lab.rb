343367cf9350a4e12907f847ca0082bd0c4590b4
Robert M Ricci <rmricci121@gmail.com>
2019-10-25 23:54:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,9 +27,8 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
     superheros = {spiderman: "Peter Parker", captainamerica: "Steve Rogers"}
-    puts superheros[:spiderman]
-    
-nil
+    puts superheros[spiderman:]
+  nil
 end
 
 
