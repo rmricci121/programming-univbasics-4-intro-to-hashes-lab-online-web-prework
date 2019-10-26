69346402eb9a8643e6d132cc4c76a22667bf7869
Robert M Ricci <rmricci121@gmail.com>
2019-10-26 00:15:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-    superheros = {spiderman => "Peter Parker", captainamerica => "Steve Rogers"}
-     superheros[captainamerica]
+    {spiderman => "Peter Parker", captainamerica => "Steve Rogers"}
+    [captainamerica]
   
 end
 
